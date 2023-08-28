94305c612dd5f3d5e4ae069e14ff32ca4a91ebda
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-08-28 16:30:53 -0300

    remoção de comentários

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 
-<!--
 
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
-
--->
 
 <html>
 
